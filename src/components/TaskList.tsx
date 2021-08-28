7add2e06061d95d9d03e54b241f40d56a4fd0f27
Rafael <rafael.gcaldas01@gmail.com>
2021-08-27 23:07:49 -0300

    Challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,44 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) {
+      return
+    }
+
+    const task: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks([...tasks, task]);
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const updatedTasks = tasks.map( task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+        return task;
+      }
+
+      return task;
+    });
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    
+    const filteredTasks = tasks.filter( task => {
+      return task.id !== id
+    });
+
+    setTasks(filteredTasks);
   }
 
   return (
